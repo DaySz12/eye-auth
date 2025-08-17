f5840f121e5431cfb563cf3b55582475073cff6c
DaySz12 <150317951+DaySz12@users.noreply.github.com>
2025-08-17 19:48:40 +0700

    Fix export statement for supabase client and ensure proper formatting

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://twjdluwmjobxtgitnyic.supabase.co"; // Project URL
-const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3amRsdXdtam9ieHRnaXRueWljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMzQ0MTIsImV4cCI6MjA3MDkxMDQxMn0.-ZLJzqElgZjDSSfqIYiPseh2scbm22gU46jhB7KDlEE" // anon public key (จากที่ copy มา)
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3amRsdXdtam9ieHRnaXRueWljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMzQ0MTIsImV4cCI6MjA3MDkxMDQxMn0.-ZLJzqElgZjDSSfqIYiPseh2scbm22gU46jhB7KDlEE"; // anon public key
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
