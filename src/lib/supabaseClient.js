7a68b505ee4f5f9350d4024801d2f9b8fbb3d63d
Nanthapong <150318144+NANTHAPONG007@users.noreply.github.com>
2025-08-17 19:44:01 +0700

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://twjdluwmjobxtgitnyic.supabase.co"; // Project URL
+const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3amRsdXdtam9ieHRnaXRueWljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMzQ0MTIsImV4cCI6MjA3MDkxMDQxMn0.-ZLJzqElgZjDSSfqIYiPseh2scbm22gU46jhB7KDlEE" // anon public key (จากที่ copy มา)
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
